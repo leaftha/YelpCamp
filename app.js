1f97bee4221b90bc006cd2ad4545ad640b8b03b1
leaftha <vltpcks@gmail.com>
2020-09-13 03:36:09 +0900

    adding mongoose and fixed bootstrap4

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+//Schema
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//   name: "Granite Hill",
+//   image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+// })
+//   .then((campground) => {
+//     console.log(campground);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 var campgrounds = [
   {
@@ -49,15 +70,29 @@ app.get("/", (req, res) => {
 });
 
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  //from DBS
+  Campground.find({})
+    .then((allCampgrounds) => {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.post("/campgrounds", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  Campground.create(newCampground)
+    .then((newDB) => {
+      res.redirect("/campgrounds");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // campgrounds.push(newCampground);
 });
 
 app.get("/campgrounds/new", (req, res) => {
